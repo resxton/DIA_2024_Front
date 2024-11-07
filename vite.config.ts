89a080724a98f3f1ba4a726dc29520993e09cdb4
resxton <gornomirov661@gmail.com>
2024-11-07 15:08:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 3000 },
+  server: { 
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/"),
+      },
+    },
+    port: 3000
+  },
   plugins: [react()],
 })
