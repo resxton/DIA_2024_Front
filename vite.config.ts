61d699e5f0a2a5110bb40a97f5918650bcfeec1c
resxton <gornomirov661@gmail.com>
2024-11-20 12:30:08 +0300

    ip for phone net

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   clearScreen: false,
   server: { 
-    host: "192.168.31.8",
+    host: "172.20.10.2",
     proxy: {
       "/api": {
-        target: "http://192.168.31.8:8000",
+        target: "http://172.20.10.2:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/"),
       },
