23e59dc860b160aae4ea13072dc280a591ebf426
resxton <gornomirov661@gmail.com>
2024-12-04 11:18:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   clearScreen: false,
   server: { 
-    host: "localhost",
+    host: "172.20.10.2",
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://172.20.10.2:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/"),
       },
